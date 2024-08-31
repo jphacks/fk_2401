49102f3299a4b52064259cc4523783095ba689d6
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-08-31 06:16:42 +0000

    Add DB connection func

---
+++ backend/internal/infrastructure/db/mysql/config.go
@@ -0,0 +1,42 @@
+package mysql
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+}
+
+func NewConfig() *Config {
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath("./")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("cannot read config: %v", err)
+	}
+
+	host := viper.GetString("MYSQL_HOSTNAME")
+	port := viper.GetString("MYSQL_PORT")
+	user := viper.GetString("MYSQL_USER")
+	password := viper.GetString("MYSQL_PASSWORD")
+	dbName := viper.GetString("MYSQL_DB")
+
+	cfg := &Config{
+		Host:     host,
+		Port:     port,
+		User:     user,
+		Password: password,
+		DBName:   dbName,
+	}
+
+	return cfg
+}
