3fc7eb1f7912bfc3e48f862229bb29b08474c254
sug-1210 <sugimoto.yu.681@s.kyushu-u.ac.jp>
2024-09-12 02:30:29 +0900

    add send_pram.go

---
+++ backend/internal/infrastructure/M304/send_param.go
@@ -0,0 +1,30 @@
+package m304
+
+import "net/http"
+
+func send_BlockA(LC_UECS_ID string,
+	LC_MAC string,
+	FIX_DHCP_FLAG string,
+	FIXED_IPADDRESS string,
+	FIXED_NETMASK string,
+	FIXED_DEFGW string,
+	FIXED_DNS string,
+	VENDER_NAME string,
+	NODE_NAME string) {
+	address := 0x0000
+
+	url := ""
+	resp, _ := http.Get(url)
+}
+
+func send_BlockB() {
+
+}
+
+func send_BlockC() {
+
+}
+
+func send_BlockD() {
+
+}
