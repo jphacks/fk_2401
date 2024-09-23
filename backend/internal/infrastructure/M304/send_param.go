7c0d596f194701e660d36a00b28f39d651d82b07
sug-1210 <sugimoto.yu.681@s.kyushu-u.ac.jp>
2024-09-23 11:04:24 +0000

    add sum funcs

--- backend/internal/infrastructure/M304/send_param.go
+++ backend/internal/infrastructure/M304/send_param.go
@@ -1,10 +1,29 @@
 package m304
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func padding(s string, out_len int, padchar string) string {
+	padding := out_len - len(s)
+	if padding > 0 {
+		return strings.Repeat(padchar, padding) + s
+	} else {
+		return s
+	}
+}
+
+func byte_arrange(n int) string {
+	hexstr := fmt.Sprintf("%x", n)
+	value := padding(hexstr, 2, "0")
+	return value
+}
 
 func send_BlockA(LC_UECS_ID string,
 	LC_MAC string,
-	FIX_DHCP_FLAG string,
+	FIX_DHCP_FLAG int,
 	FIXED_IPADDRESS string,
 	FIXED_NETMASK string,
 	FIXED_DEFGW string,
@@ -12,6 +31,14 @@ func send_BlockA(LC_UECS_ID string,
 	VENDER_NAME string,
 	NODE_NAME string) {
 	address := 0x0000
+	ih_uecs_id := padding(LC_UECS_ID, 12, "0")
+	ih_mac := strings.Replace(LC_MAC, ":", "", -1)
+	ih_dhcpflg := byte_arrange(FIX_DHCP_FLAG)
+	sp_ip_addr := strings.Split(FIXED_IPADDRESS, ".")
+	ih_ip_addr := ""
+	for _, v := range sp_ip_addr {
+		ih_ip_addr += byte_arrange(int(v))
+	}
 
 	url := ""
 	resp, _ := http.Get(url)
