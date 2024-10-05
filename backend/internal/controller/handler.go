424e3c41f40433c4c4f8f6af027e405d462bc241
sug-1210 <sugimoto.yu.681@s.kyushu-u.ac.jp>
2024-10-06 05:56:33 +0900

    add create house controller

--- backend/internal/controller/handler.go
+++ backend/internal/controller/handler.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Fumiya-Tahara/uecs-navi.git/internal/controller/generated"
 	"github.com/Fumiya-Tahara/uecs-navi.git/internal/usecase/service"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type Handler struct {
@@ -30,8 +32,35 @@ func (h Handler) GetHouses(c *gin.Context) {
 	c.JSON(http.StatusOK, houses)
 }
 
-func (h Handler) CreateHouse(c *gin.Context) {
+type CreateHouseController struct {
+	Name string `json:"name" binding:"required,min=1,max=10,alphanum"`
+}
 
+func (h Handler) CreateHouse(c *gin.Context) {
+	var json CreateHouseController
+	if err := c.BindJSON(&json); err != nil {
+
+		var errorMessages []string
+		if errs, ok := err.(validator.ValidationErrors); ok {
+			for _, e := range errs {
+				errorMessages = append(errorMessages, fmt.Sprintf("Field %s is %s", e.Field(), e.ActualTag()))
+			}
+		} else {
+			errorMessages = append(errorMessages, err.Error())
+		}
+
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	name := json.Name
+	id, err := h.houseService.CreateHouse(name)
+
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, id)
 }
 
 func (h Handler) GetDevice(c *gin.Context, houseId int) {
