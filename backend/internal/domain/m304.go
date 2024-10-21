6060672e9c603daf7d9260260b7959d7f1928cf2
sug-1210 <sugimoto.yu.681@s.kyushu-u.ac.jp>
2024-10-22 03:12:54 +0900

    fix m304 domain

--- backend/internal/domain/m304.go
+++ backend/internal/domain/m304.go
@@ -2,26 +2,26 @@ package domain
 
 type M304 struct {
 	ID         int
-	UecsID     string
-	MacAddr    string
+	UecsID     *string
+	MacAddr    *string
 	DhcpFlg    bool
-	IpAddr     string
-	NetMask    string
-	Defgw      string
-	Dns        string
-	VenderName string
-	NodeName   string
-	Rly0       int
-	Rly1       int
-	Rly2       int
-	Rly3       int
-	Rly4       int
-	Rly5       int
-	Rly6       int
-	Rly7       int
+	IpAddr     *string
+	NetMask    *string
+	Defgw      *string
+	Dns        *string
+	VenderName *string
+	NodeName   *string
+	Rly0       *int
+	Rly1       *int
+	Rly2       *int
+	Rly3       *int
+	Rly4       *int
+	Rly5       *int
+	Rly6       *int
+	Rly7       *int
 }
 
-func NewM304(uecsID string, macAddr string, dhcpFlg bool, ipAddr string, netMask string, defgw string, dns string, venderName string, nodeName string, rly0 int, rly1 int, rly2 int, rly3 int, rly4 int, rly5 int, rly6 int, rly7 int) *M304 {
+func NewM304(uecsID *string, macAddr *string, dhcpFlg bool, ipAddr *string, netMask *string, defgw *string, dns *string, venderName *string, nodeName *string, rly0 *int, rly1 *int, rly2 *int, rly3 *int, rly4 *int, rly5 *int, rly6 *int, rly7 *int) *M304 {
 	return &M304{
 		UecsID:     uecsID,
 		MacAddr:    macAddr,
@@ -43,7 +43,7 @@ func NewM304(uecsID string, macAddr string, dhcpFlg bool, ipAddr string, netMask
 	}
 }
 
-func NewM304WithID(id int, uecsID string, macAddr string, dhcpFlg bool, ipAddr string, netMask string, defgw string, dns string, venderName string, nodeName string, rly0 int, rly1 int, rly2 int, rly3 int, rly4 int, rly5 int, rly6 int, rly7 int) *M304 {
+func NewM304WithID(id int, uecsID *string, macAddr *string, dhcpFlg bool, ipAddr *string, netMask *string, defgw *string, dns *string, venderName *string, nodeName *string, rly0 *int, rly1 *int, rly2 *int, rly3 *int, rly4 *int, rly5 *int, rly6 *int, rly7 *int) *M304 {
 	return &M304{
 		ID:         id,
 		UecsID:     uecsID,
