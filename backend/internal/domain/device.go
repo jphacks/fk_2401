a88bc15e7b8cf72b13302a010b701ed8aa852ce0
sug-1210 <sugimoto.yu.681@s.kyushu-u.ac.jp>
2024-10-24 19:36:34 +0000

    fix device domain

--- backend/internal/domain/device.go
+++ backend/internal/domain/device.go
@@ -4,30 +4,43 @@ type Device struct {
 	ID            int
 	HouseID       int
 	ClimateDataID int
+	UecsDeviceID  int
 	DeviceName    *string
+	Valid         *bool
 	SetPoint      *float64
 	Duration      *int
+	Operator      *int
 }
 
-func NewDeviceWithID(id int, houseID int, climateDataID int, deviceName *string, setPoint *float64, duration *int) *Device {
+func NewDeviceWithID(id int, houseID int, climateDataID int, uecsDeviceID int, deviceName *string, valid *bool, setPoint *float64, duration *int, operator *int) *Device {
 	return &Device{
 		ID:            id,
 		HouseID:       houseID,
 		ClimateDataID: climateDataID,
+		UecsDeviceID:  uecsDeviceID,
 		DeviceName:    deviceName,
+		Valid:         valid,
 		SetPoint:      setPoint,
 		Duration:      duration,
+		Operator:      operator,
 	}
 }
 
-func NewDevice(houseID int, climateDataID int, deviceName *string, setPoint *float64, duration *int) *Device {
+func NewDevice(houseID int, climateDataID int, uecsDeviceID int, deviceName *string, valid *bool, setPoint *float64, duration *int, operator *int) *Device {
 	return &Device{
 		HouseID:       houseID,
 		ClimateDataID: climateDataID,
+		UecsDeviceID:  uecsDeviceID,
 		DeviceName:    deviceName,
+		Valid:         valid,
 		SetPoint:      setPoint,
 		Duration:      duration,
+		Operator:      operator,
 	}
+}
+
+func (device *Device) ChangeValid(valid *bool) {
+	device.Valid = valid
 }
 
 func (device *Device) ChangeSetPoint(setPoint *float64) {
@@ -36,4 +49,8 @@ func (device *Device) ChangeSetPoint(setPoint *float64) {
 
 func (device *Device) ChangeDuration(duration *int) {
 	device.Duration = duration
+}
+
+func (device *Device) ChangeOperator(operator *int) {
+	device.Operator = operator
 }
