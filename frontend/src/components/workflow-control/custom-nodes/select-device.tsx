effc6981f514dc046589338bd49d90259d322b5c
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-11-03 12:19:13 +0000

    Fix start node

---
+++ frontend/src/components/workflow-control/custom-nodes/select-device.tsx
@@ -0,0 +1,72 @@
+import {
+  Box,
+  Divider,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
+import { Handle, Position, Node, NodeProps } from "@xyflow/react";
+import { AddNodeFunction } from "../workflow";
+import { useState } from "react";
+
+interface SelectDeviceNodeData {
+  [key: string]: unknown;
+  addNode: AddNodeFunction;
+}
+
+type SelectDeviceNodePropsType = Node<SelectDeviceNodeData>;
+
+type SelectDeviceNodeProps = NodeProps<SelectDeviceNodePropsType>;
+
+export const SelectDeviceNode = ({ id, data }: SelectDeviceNodeProps) => {
+  const { addNode } = data;
+  const [selectedDevice, setSelectedDevice] = useState<number>(0);
+  const handleSelectedDeviceChange = (event: SelectChangeEvent) => {
+    const selectedDeviceID: number = parseInt(event.target.value, 10);
+    setSelectedDevice(selectedDeviceID);
+  };
+
+  return (
+    <Box
+      sx={{
+        border: "1px solid #000",
+        borderRadius: "10px",
+        backgroundColor: "#FFF",
+      }}
+    >
+      <Box sx={{ padding: "8px" }}>
+        <Typography variant="h6">Select Device</Typography>
+        <Divider />
+        <Box sx={{ padding: "8px", display: "flex", justifyContent: "center" }}>
+          <Box sx={{ flex: 2 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">デバイス</InputLabel>
+              <Select
+                value={selectedDevice.toString()}
+                size="small"
+                onChange={handleSelectedDeviceChange}
+                label="デバイス"
+                inputProps={{ className: "nodrag nopan nowheel" }}
+              >
+                <MenuItem value={1}>{"加温器1"}</MenuItem>
+                <MenuItem value={2}>{"窓開閉装置1"}</MenuItem>
+                <MenuItem value={3}>{"加温器2"}</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Box>
+      </Box>
+      <Handle
+        position={Position.Right}
+        type="source"
+        onClick={(event) => {
+          event.stopPropagation();
+          addNode(id);
+        }}
+      />
+    </Box>
+  );
+};
