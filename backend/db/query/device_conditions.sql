a8753819d681fb1fede3242e2a973535b59fea70
sug-1210 <sugimoto.yu.681@s.kyushu-u.ac.jp>
2024-11-11 21:02:19 +0900

    fix device condition

--- backend/db/query/device_conditions.sql
+++ backend/db/query/device_conditions.sql
@@ -1,13 +1,13 @@
 -- name: GetDeviceConditionsFromDeviceID :many
-SELECT id, device_id, valid, set_point, duration, operator
+SELECT id, device_id, operation_id, valid, set_point, duration, operator
 FROM device_conditions
 WHERE device_id = ?;
 
 -- name: GetDeviceConditionFromID :one
-SELECT id, device_id, valid, set_point, duration, operator
+SELECT id, device_id, operation_id, valid, set_point, duration, operator
 FROM device_conditions
 WHERE id = ?;
 
 -- name: CreateDeviceCondition :execlastid
-INSERT INTO device_conditions (device_id, valid, set_point, duration, operator)
-VALUES (?, ?, ?, ?, ?);
+INSERT INTO device_conditions (device_id, operation_id, valid, set_point, duration, operator)
+VALUES (?, ?, ?, ?, ?, ?);
