79601ebf0c765c5d6e94d6ca491884430fd31ab8
sug-1210 <sugimoto.yu.681@s.kyushu-u.ac.jp>
2024-11-15 23:41:48 +0900

    add controller

--- backend/internal/controller/dto.go
+++ backend/internal/controller/dto.go
@@ -32,6 +32,92 @@ type ClimateDataResponse struct {
 	Unit        string `json:"unit,omitempty"`
 }
 
+// EdgeRequest defines model for EdgeRequest.
+type EdgeRequest struct {
+	SourceNodeId string `json:"source_node_id,omitempty"`
+	TargetNodeId string `json:"target_node_id,omitempty"`
+}
+
+// EdgeResponse defines model for EdgeResponse.
+type EdgeResponse struct {
+	Id           int    `json:"id,omitempty"`
+	SourceNodeId string `json:"source_node_id,omitempty"`
+	TargetNodeId string `json:"target_node_id,omitempty"`
+	WorkflowId   int    `json:"workflow_id,omitempty"`
+}
+
+// NodeRequest defines model for NodeRequest.
+type NodeRequest struct {
+	Data           map[string]interface{} `json:"data,omitempty"`
+	PositionX      float32                `json:"position_x,omitempty"`
+	PositionY      float32                `json:"position_y,omitempty"`
+	Type           string                 `json:"type,omitempty"`
+	WorkflowNodeId string                 `json:"workflow_node_id,omitempty"`
+}
+
+// NodeResponse defines model for NodeResponse.
+type NodeResponse struct {
+	Data           map[string]interface{} `json:"data,omitempty"`
+	Id             int                    `json:"id,omitempty"`
+	PositionX      float32                `json:"position_x,omitempty"`
+	PositionY      float32                `json:"position_y,omitempty"`
+	NodeType       string                 `json:"type,omitempty"`
+	WorkflowId     int                    `json:"workflow_id,omitempty"`
+	WorkflowNodeId string                 `json:"workflow_node_id,omitempty"`
+}
+
+// TimeSchedule defines model for TimeSchedule.
+type TimeSchedule struct {
+	// EndTime 終了時刻（HH:mm形式）
+	EndTime string `json:"end_time,omitempty"`
+
+	// StartTime 開始時刻（HH:mm形式）
+	StartTime string            `json:"start_time,omitempty"`
+	Workflows []WorkflowRequest `json:"workflows,omitempty"`
+}
+
+// TimeScheduleRequest defines model for TimeScheduleRequest.
+type TimeScheduleRequest struct {
+	IpAddress    string         `json:"ip_address,omitempty"`
+	TimeSchedule []TimeSchedule `json:"time_schedule,omitempty"`
+}
+
+// WorkflowRequest defines model for WorkflowRequest.
+type WorkflowRequest struct {
+	DeviceId int    `json:"device_id,omitempty"`
+	Name     string `json:"name,omitempty"`
+}
+
+// WorkflowResponse defines model for WorkflowResponse.
+type WorkflowResponse struct {
+	Id   int    `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+}
+
+// WorkflowUIRequest defines model for WorkflowUIRequest.
+type WorkflowUIRequest struct {
+	Edges []EdgeRequest `json:"edges,omitempty"`
+	Nodes []NodeRequest `json:"nodes,omitempty"`
+}
+
+// WorkflowUIResponse defines model for WorkflowUIResponse.
+type WorkflowUIResponse struct {
+	Edges []EdgeResponse `json:"edges,omitempty"`
+	Nodes []NodeResponse `json:"nodes,omitempty"`
+}
+
+// WorkflowWithUIRequest defines model for WorkflowWithUIRequest.
+type WorkflowWithUIRequest struct {
+	Workflow   WorkflowRequest   `json:"workflow,omitempty"`
+	WorkflowUI WorkflowUIRequest `json:"workflow_ui,omitempty"`
+}
+
+// WorkflowWithUIResponse defines model for WorkflowWithUIResponse.
+type WorkflowWithUIResponse struct {
+	Workflow   WorkflowResponse   `json:"workflow,omitempty"`
+	WorkflowUI WorkflowUIResponse `json:"workflow_ui,omitempty"`
+}
+
 func NewHouseResponse(id int, name string) *HouseResponse {
 	return &HouseResponse{
 		ID:   id,
@@ -64,5 +150,54 @@ func NewClimateDataResponse(id int, climateData, unit string) *ClimateDataRespon
 		ID:          id,
 		ClimateData: climateData,
 		Unit:        unit,
+	}
+}
+
+func NewWorkflowResponse(id int, name string) *WorkflowResponse {
+	return &WorkflowResponse{
+		Id:   id,
+		Name: name,
+	}
+}
+
+func NewWorkFlowWithUIResponse(workFlow WorkflowResponse, workFlowUI WorkflowUIResponse) *WorkflowWithUIResponse {
+	return &WorkflowWithUIResponse{
+		Workflow:   workFlow,
+		WorkflowUI: workFlowUI,
+	}
+}
+
+func NewWorkFlowUIResponse(edges []EdgeResponse, nodes []NodeResponse) *WorkflowUIResponse {
+	return &WorkflowUIResponse{
+		Edges: edges,
+		Nodes: nodes,
+	}
+}
+
+func NewEdgeResponse(id int, sourceNodeID string, targetNodeID string, workFlowID int) *EdgeResponse {
+	return &EdgeResponse{
+		Id:           id,
+		SourceNodeId: sourceNodeID,
+		TargetNodeId: targetNodeID,
+		WorkflowId:   workFlowID,
+	}
+}
+
+func NewNodeResponse(
+	data map[string]interface{},
+	id int, positionX float32,
+	positionY float32,
+	nodeType string,
+	workFlowID int,
+	workFlowNodeID string,
+) *NodeResponse {
+	return &NodeResponse{
+		Data:           data,
+		Id:             id,
+		PositionX:      positionX,
+		PositionY:      positionY,
+		NodeType:       nodeType,
+		WorkflowId:     workFlowID,
+		WorkflowNodeId: workFlowNodeID,
 	}
 }
--- backend/internal/controller/handler.go
+++ backend/internal/controller/handler.go
@@ -1,9 +1,15 @@
 package controller
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
+	"github.com/Fumiya-Tahara/uecs-navi.git/internal/infrastructure/orm/mysqlc"
 	"github.com/Fumiya-Tahara/uecs-navi.git/internal/usecase/service"
 	"github.com/gin-gonic/gin"
 )
@@ -128,4 +134,189 @@ func (h Handler) GetClimateData(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, climateDataRes)
+}
+
+func (h Handler) CreateAndBuildTimeSchedule(c *gin.Context) {
+	var json TimeScheduleRequest
+	if err := c.BindJSON(&json); err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"status": "BadRequest"})
+		return
+	}
+	setStartTime := json.TimeSchedule[0].StartTime
+	setEndTime := json.TimeSchedule[0].EndTime
+	StartTime := strings.Split(setStartTime, ":")
+	EndTime := strings.Split(setEndTime, ":")
+	sthr, err := strconv.Atoi(StartTime[0])
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	stmn, err := strconv.Atoi(StartTime[1])
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	edhr, err := strconv.Atoi(EndTime[0])
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	edmn, err := strconv.Atoi(EndTime[0])
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	timeSchedules := json.TimeSchedule
+	var deviceIDs []int
+	for _, v := range timeSchedules {
+		workFlows := v.Workflows
+		for _, w := range workFlows {
+			deviceIDs = append(deviceIDs, w.DeviceId)
+		}
+	}
+	var rly []int
+	for _, v := range deviceIDs {
+		device, err := h.deviceService.GetDeviceFromID(v)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		rly = append(rly, *device.Rly)
+	}
+
+	unique := make(map[int]bool) // ユニークな要素を記録
+	var result []int             // 最終的な結果
+
+	for _, num := range rly {
+		// 0~7の範囲内かつまだ登録されていない場合に追加
+		if num >= 0 && num <= 7 && !unique[num] {
+			unique[num] = true
+			result = append(result, num)
+		}
+	}
+	demoData := service.DemoData{
+		IpAddr: json.IpAddress,
+		StHr:   sthr,
+		StMn:   stmn,
+		EdHr:   edhr,
+		EdMn:   edmn,
+		InMn:   1,
+		DuMn:   1,
+		Rly:    result,
+	}
+	id, err := service.BuildDemoM304(&demoData)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	c.JSON(http.StatusOK, id)
+}
+
+func (h Handler) GetWorkflows(c *gin.Context) {
+	ctx := context.Background()
+
+	workFlows, err := mysqlc.GetAllWorkflows(ctx)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "InternalSeverError"})
+		return
+	}
+
+	workFlowsRes := make([]*WorkflowResponse, len(workFlows))
+	for i, v := range workFlows {
+		workFlowsRes[i] = NewWorkflowResponse(int(v.ID), v.Name)
+	}
+
+	c.JSON(http.StatusOK, workFlowsRes)
+}
+
+func (h Handler) GetWorkflowsWithUI(c *gin.Context) {
+	ctx := context.Background()
+
+	workFlows, err := mysqlc.GetAllWorkflows(ctx)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "InternalSeverError"})
+		return
+	}
+
+	workFlowsUIRes := make([]*WorkflowWithUIResponse, len(workFlows))
+	for i, v := range workFlows {
+		edges, err := mysqlc.GetEdgesFromWorkflow(ctx, int32(v.ID))
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"status": "InternalSeverError"})
+			return
+		}
+		edgeRes := make([]EdgeResponse, len(edges))
+		for j, w := range edges {
+			edgeRes[j] = *NewEdgeResponse(w.Id, w.SourceNodeID, w.TargetNodeID, int(w.WorkflowId))
+		}
+		nodes, err := mysqlc.GetNodesFromWorkflow(ctx, int32(v.ID))
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"status": "InternalSeverError"})
+			return
+		}
+		nodeRes := make([]NodeResponse, len(nodes))
+		for j, w := range nodes {
+			nodeRes[j] = *NewNodeResponse(w.Data, w.Id, w.PositionX, w.PositionY, w.NodeType, int(w.WorkflowId), w.WorkflowNodeID)
+		}
+		workFlowUI := NewWorkFlowUIResponse(edgeRes, nodeRes)
+		workFlowsUIRes[i] = NewWorkFlowWithUIResponse(v, *workFlowUI)
+	}
+
+	c.JSON(http.StatusOK, workFlowsUIRes)
+}
+
+func (h Handler) CreateWorkflowWithUI(c *gin.Context) {
+	ctx := context.Background()
+
+	var jsonWorkflow WorkflowWithUIRequest
+	if err := c.BindJSON(&jsonWorkflow); err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"status": "BadRequest"})
+		return
+	}
+	workFlowName := jsonWorkflow.Workflow
+	workFlowID, err := mysqlc.CreateWorkflow(ctx, workFlowName)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"status": "BadRequest"})
+		return
+	}
+	workFlowUIEdges := jsonWorkflow.WorkflowUI.Edges
+	workFlowUINodes := jsonWorkflow.WorkflowUI.Nodes
+	for _, v := range workFlowUIEdges {
+		edgeReq := mysqlc.CreateEdgeParams{
+			WorkflowID:   int32(workFlowID),
+			SourceNodeID: v.SourceNodeId,
+			TargetNodeID: v.TargetNodeId,
+		}
+		_, err := mysqlc.CreateEdge(ctx, edgeReq)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"status": "BadRequest"})
+			return
+		}
+	}
+	for _, v := range workFlowUINodes {
+		rawData, err := json.Marshal(v.Data)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"status": "BadRequest"})
+			return
+		}
+		nodeReq := mysqlc.CreateNodeParams{
+			WorkflowID: int32(workFlowID),
+			Type:       v.Type,
+			Data:       json.RawMessage(rawData),
+			PositionX:  float64(v.PositionX),
+			PositionY:  float64(v.PositionY),
+		}
+		_, err = mysqlc.CreateNode(ctx, nodeReq)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"status": "BadRequest"})
+			return
+		}
+	}
+	c.JSON(http.StatusOK, workFlowID)
 }
